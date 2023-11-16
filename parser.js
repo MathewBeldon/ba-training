ea01e21460d5f26ab3d4e0afe7f69906353fa4d5
Tash Musanhu <tashinga.musanhu@and.digital>
2023-11-16 15:15:52 +0000

    add parser + error handling

--- parser.js
+++ parser.js
@@ -1,32 +1,108 @@
-const fs = require('fs');
-const path = require('path');
+const fsPromises = require("fs").promises;
+const fs = require("fs");
+const path = require("path");
 
-const directoryPath = './data'; // Replace with the path to your directory
+const finalOutputArray = [];
 
-function readAndParseJsonFile(filePath) {
+//const directoryPath = '/data'; -- Replace with the path to your directory
+const directoryPath = path.join(__dirname, "data");
+
+async function readAndParseJsonFile(filePath) {
+	try {
+		// Read the content of the file
+		const fileContent = await fsPromises.readFile(filePath, "utf-8");
+
+		// Parse the JSON data
+		const jsonData = JSON.parse(fileContent);
+
+		return jsonData;
+	} catch (error) {
+		console.error(
+			`Error reading or parsing the JSON file at ${filePath}:`,
+			error
+		);
+	}
 }
 
 function processInputData(inputData) {
+	let reversedData = inputData.data.reverse();
+
+	const createMessage = (role, content) => {
+		return {
+			role,
+			content
+		};
+	};
+
+	/* 
+    Note: we assume that each content list 
+    will only have 1 object. Is this always the case?
+    */
+	const messages = reversedData.map((input) => {
+		return createMessage(input.role, input.content[0].text.value);
+	});
+
+	const jsonLFormat = { messages: messages };
+
+	return jsonLFormat;
 }
 
 // Main function to execute the processing
 function main() {
-    fs.readdir(directoryPath, (err, files) => {
-        if (err) {
-            console.log('Error getting directory information.');
-        } else {
-            files.forEach(file => {
-                if (path.extname(file) === '.json') {
-                    const filePath = path.join(directoryPath, file);
+	fs.readdir(directoryPath, (err, files) => {
+		if (err) {
+			console.error("Error getting directory information.", err);
+		} else {
+			files.forEach((file) => {
+				if (path.extname(file) === ".json") {
+
+					const filePath = path.join(directoryPath, file);
                     const inputData = readAndParseJsonFile(filePath);
 
                     const outputData = processInputData(inputData);
                     
                     console.log(JSON.stringify(outputData, null, 2));
-                }
-            });
-        }
-    });
+				}
+			});
+		}
+	});
 }
 
-main();
+async function mainT() {
+	try {
+		const files = await fsPromises.readdir(directoryPath);
+
+		for (const file of files) {
+			if (path.extname(file) === ".json") {
+
+				const filePath = path.join(directoryPath, file);
+
+                // Read the content of the file
+                const fileContent = await fsPromises.readFile(filePath, "utf-8");
+
+                //check not empty
+                if (!fileContent.trim()) {
+                    console.warn(`Warning: Empty JSON file at ${filePath}`);
+                    continue;
+                }
+
+				const inputData = await readAndParseJsonFile(filePath);
+
+				const messages = await processInputData(inputData);
+
+				finalOutputArray.push(messages);
+
+				console.log("Successfully added message stream from file: " + file);
+			}
+		}
+	} catch (err) {
+		console.error("Error getting directory information.", err);
+	}
+}
+
+
+mainT().then(() => {
+	console.log(finalOutputArray);
+
+    console.log(finalOutputArray[0])
+});
